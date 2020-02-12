2d7c85cc48034136062235ef1b69a0ae1dab7fd0
Miguel <ubathfenome@gmail.com>
2020-02-12 23:06:53 +0100

    Dialogo info sensor. Fix #23
    Signed-off-by: Miguel <ubathfenome@gmail.com>

--- app/src/main/java/tfm/uniovi/pirateseas/controller/sensors/events/SensorEventAdapter.java
+++ app/src/main/java/tfm/uniovi/pirateseas/controller/sensors/events/SensorEventAdapter.java
@@ -2,15 +2,14 @@ package tfm.uniovi.pirateseas.controller.sensors.events;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.graphics.Paint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 
@@ -25,7 +24,6 @@ public class SensorEventAdapter extends ArrayAdapter<AppSensorEvent> {
 
     private class AppSensorEventViewHolder {
         private TextView txtSensorName;
-        private CheckBox chkActive;
         private TextView txtEventName;
         private ImageView imgEventThumbnail;
     }
@@ -45,61 +43,59 @@ public class SensorEventAdapter extends ArrayAdapter<AppSensorEvent> {
 
         final AppSensorEventViewHolder viewHolder =new AppSensorEventViewHolder();
         viewHolder.txtSensorName = convertView.findViewById(R.id.txtSensorName);
-        viewHolder.chkActive = convertView.findViewById(R.id.chkActive);
         viewHolder.txtEventName = convertView.findViewById(R.id.txtEventName);
         viewHolder.imgEventThumbnail = convertView.findViewById(R.id.imgEventThumbnail);
 
         final View.OnClickListener sensorClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mSensorEvents.get(position).isActive()) {
-                    Toast.makeText(mContext, mContext.getString(R.string.toast_event_enabled,
-                            viewHolder.txtSensorName.getText().toString(),
-                            viewHolder.txtEventName.getText().toString()),
-                            Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(mContext, mContext.getString(R.string.toast_event_disabled,
-                            viewHolder.txtSensorName.getText().toString(),
-                            viewHolder.txtEventName.getText().toString()),
-                            Toast.LENGTH_LONG).show();
-                }
+                createEventDialog(viewHolder.txtSensorName.getText().toString(), 0, mContext.getString(R.string.toast_event_enabled,
+                        viewHolder.txtSensorName.getText().toString(),
+                        viewHolder.txtEventName.getText().toString()));
             }
         };
 
         View.OnClickListener eventClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
-                LayoutInflater inflater = ((SensorActivity)mContext).getLayoutInflater();
-                View inflatedView = inflater.inflate(R.layout.event_dialog, null);
-                TextView txtEventTitle = inflatedView.findViewById(R.id.txtEventTitle);
-                txtEventTitle.setText(viewHolder.txtEventName.getText().toString());
-                ImageView imgEvent = inflatedView.findViewById(R.id.imgEvent);
-                imgEvent.setImageResource(mSensorEvents.get(position).getImageResource());
-                TextView txtEventMessage = inflatedView.findViewById(R.id.txtEventMessage);
-                txtEventMessage.setText(mSensorEvents.get(position).getMessageResource());
-                Button btnEventDialogOk = inflatedView.findViewById(R.id.btnEventDialogOk);
-                final AlertDialog dialog = builder.setView(inflatedView).create();
-                btnEventDialogOk.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        dialog.dismiss();
-                    }
-                });
-                dialog.show();
+                createEventDialog(viewHolder.txtEventName.getText().toString(),
+                        mSensorEvents.get(position).getImageResource(),
+                        mContext.getString(mSensorEvents.get(position).getMessageResource()));
             }
         };
 
         viewHolder.txtSensorName.setText(mSensorEvents.get(position).getSensorName());
+        viewHolder.txtSensorName.setPaintFlags(mSensorEvents.get(position).isActive()? (viewHolder.txtSensorName.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG) : (viewHolder.txtSensorName.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG)));
         viewHolder.txtSensorName.setOnClickListener(sensorClickListener);
-        viewHolder.chkActive.setChecked(mSensorEvents.get(position).isActive());
-        viewHolder.chkActive.setEnabled(false);
-        viewHolder.chkActive.setOnClickListener(sensorClickListener);
+
         viewHolder.txtEventName.setText(mSensorEvents.get(position).getEventName());
         viewHolder.txtEventName.setOnClickListener(eventClickListener);
+
         viewHolder.imgEventThumbnail.setImageResource(mSensorEvents.get(position).getThumbnailResource());
         viewHolder.imgEventThumbnail.setOnClickListener(eventClickListener);
 
         return convertView;
+    }
+
+    private void createEventDialog(String eventTitle, int imageResource, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+        LayoutInflater inflater = ((SensorActivity)mContext).getLayoutInflater();
+        View inflatedView = inflater.inflate(R.layout.event_dialog, null);
+        TextView txtEventTitle = inflatedView.findViewById(R.id.txtEventTitle);
+        txtEventTitle.setText(eventTitle);
+        ImageView imgEvent = inflatedView.findViewById(R.id.imgEvent);
+        imgEvent.setImageResource(imageResource);
+        TextView txtEventMessage = inflatedView.findViewById(R.id.txtEventMessage);
+        txtEventMessage.setText(message);
+        Button btnEventDialogOk = inflatedView.findViewById(R.id.btnEventDialogOk);
+
+        final AlertDialog dialog = builder.setView(inflatedView).create();
+        btnEventDialogOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
